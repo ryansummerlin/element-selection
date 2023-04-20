26185ae5a23b9317b099963ba2f7915aeed28c04
ryansummerlin <ryansummerlin777@gmail.com>
2023-04-19 21:48:10 -0700

    Finished all queries

--- selectors.js
+++ selectors.js
@@ -3,37 +3,37 @@ const select = () => {
 
     /* Section 1 */
     // 1. Get all seeded fruit elements
-    // const seededFruits = document.querySelectorAll('.seed');
-    // seededFruits.forEach(el => console.log(el));
+    const seededFruits = document.querySelectorAll('.seed');
+    seededFruits.forEach(el => console.log(el));
 
     // 2. Get all seedless fruit elements
-    // const seedlessFruits = document.querySelectorAll('.seedless');
-    // seedlessFruits.forEach(el => console.log(el));
+    const seedlessFruits = document.querySelectorAll('.seedless');
+    seedlessFruits.forEach(el => console.log(el));
 
     // 3. Get first seedless fruit element
-    // const firstSeedless = document.getElementsByClassName('seedless')[0];
-    // console.log(firstSeedless);
+    const firstSeedless = document.getElementsByClassName('seedless')[0];
+    console.log(firstSeedless);
 
     /* Section 2 */
     // 4. Get inner span with text "you"
-    // const allSpanElements = document.querySelectorAll('span');
-    // allSpanElements.forEach(span => {
-    //     if (span.innerText === 'you') {
-    //         console.log(span);
-    //     }
-    // });
+    const allSpanElements = document.querySelectorAll('span');
+    allSpanElements.forEach(span => {
+        if (span.innerText === 'you') {
+            console.log(span);
+        }
+    });
 
     // 5. Get all children of element "wrapper"
-    // const wrappers = document.getElementById("wrapper");
-    // const wrapperChildren = wrappers.children;
-    // for (let i = 0; i < wrapperChildren.length; i++) {
-    //     console.log(wrapperChildren[i]);
-    // }
+    const wrappers = document.getElementById("wrapper");
+    const wrapperChildren = wrappers.children;
+    for (let i = 0; i < wrapperChildren.length; i++) {
+        console.log(wrapperChildren[i]);
+    }
 
 
     // 6. Get all odd number list items in the list
-    // const oddListItems = document.querySelectorAll('li.odd');
-    // oddListItems.forEach(li => console.log(li));
+    const oddListItems = document.querySelectorAll('li.odd');
+    oddListItems.forEach(li => console.log(li));
 
     // 7. Get all even number list items in the list
     const orderedList = document.getElementsByTagName("ol");
@@ -45,17 +45,25 @@ const select = () => {
         }
     }
 
-
-
     /* Section 3 */
     // 8. Get all tech companies without a class name
-    // Your code here
+    const techNoClassName = document.querySelectorAll("a:not([class])");
+    console.log(techNoClassName[0].innerText);
 
     // 9. Get "Amazon" list element
-    // Your code here
+    const amazon = document.querySelectorAll("a.shopping")[0];
+    console.log(amazon);
 
     // 10. Get all unicorn list elements (not the image element)
-    // Your code here
+    const listElements = document.querySelectorAll("li");
+    let listIndices = [];
+    for (let i = 0; i < listElements.length; i++) {
+        let li = listElements[i];
+        if (li.querySelectorAll("img.unicorn").length > 0) {
+            listIndices.push(i);
+        }
+    }
+    listIndices.forEach(i => console.log(listElements[i]));
 }
 
 window.onload = select;

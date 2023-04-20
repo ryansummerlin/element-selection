53ccd82237154c71e47e5c089babed44ea6e9efb
ryansummerlin <ryansummerlin777@gmail.com>
2023-04-19 19:57:10 -0700

    Created the first 7 queries, 3 more to go

--- selectors.js
+++ selectors.js
@@ -3,26 +3,49 @@ const select = () => {
 
     /* Section 1 */
     // 1. Get all seeded fruit elements
-    // Your code here
-    
+    // const seededFruits = document.querySelectorAll('.seed');
+    // seededFruits.forEach(el => console.log(el));
+
     // 2. Get all seedless fruit elements
-    // Your code here
+    // const seedlessFruits = document.querySelectorAll('.seedless');
+    // seedlessFruits.forEach(el => console.log(el));
 
     // 3. Get first seedless fruit element
-    // Your code here
+    // const firstSeedless = document.getElementsByClassName('seedless')[0];
+    // console.log(firstSeedless);
 
     /* Section 2 */
     // 4. Get inner span with text "you"
-    // Your code here
+    // const allSpanElements = document.querySelectorAll('span');
+    // allSpanElements.forEach(span => {
+    //     if (span.innerText === 'you') {
+    //         console.log(span);
+    //     }
+    // });
 
     // 5. Get all children of element "wrapper"
-    // Your code here
+    // const wrappers = document.getElementById("wrapper");
+    // const wrapperChildren = wrappers.children;
+    // for (let i = 0; i < wrapperChildren.length; i++) {
+    //     console.log(wrapperChildren[i]);
+    // }
+
 
     // 6. Get all odd number list items in the list
-    // Your code here
+    // const oddListItems = document.querySelectorAll('li.odd');
+    // oddListItems.forEach(li => console.log(li));
 
     // 7. Get all even number list items in the list
-    // Your code here
+    const orderedList = document.getElementsByTagName("ol");
+    const olChildren = orderedList[0].children;
+    for (let i = 0; i < olChildren.length; i++) {
+        let child = olChildren[i];
+        if (child.className !== 'odd') {
+            console.log(child);
+        }
+    }
+
+
 
     /* Section 3 */
     // 8. Get all tech companies without a class name
